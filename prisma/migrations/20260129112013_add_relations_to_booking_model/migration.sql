b25f084052f02c311512187596f58128a42852ab
Ashish Singh Negi <ashishamonegi@gmail.com>
2026-01-29 16:57:12 +0530

    add: hotel, room model relations to booking model

---
+++ prisma/migrations/20260129112013_add_relations_to_booking_model/migration.sql
@@ -0,0 +1,5 @@
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_hotelId_fkey" FOREIGN KEY ("hotelId") REFERENCES "Hotel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
